2d06bd352b9dfb51a2f8eaf72a2f9f5c64d23a4e
Hiran Ferreti Baccos <hiranjc1@outlook.com>
2025-12-10 16:34:26 -0300

    first commit

---
+++ src/exceptions/BusinessException.java
@@ -0,0 +1,8 @@
+package exceptions;
+
+public class BusinessException extends Exception {
+	private static final long serialVersionUID = 1L;
+	public BusinessException(String msg) {
+		super(msg);
+	}
+}
